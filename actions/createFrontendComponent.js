73a109b64a9ee13e56177b1b08cf80e9bbb971f1
CGMGMT <chosenglobalmgmt@gmail.com>
2025-04-07 02:42:05 +0000

    Fix: proper REPO_URL formatting and full clone path

--- actions/createFrontendComponent.js
+++ actions/createFrontendComponent.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const simpleGit = require('simple-git');
 
-const REPO_URL = process.env.GITHUB_REPO;
+const REPO_URL = `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_USERNAME}/${process.env.GITHUB_REPO}.git`;
 const REPO_DIR = './temp-frontend-repo';
 const FILE_DIR = 'src';
 const USER_NAME = process.env.GITHUB_USERNAME;
@@ -21,12 +21,12 @@ const ${componentName}Frontend = () => {
 };
 
 export default ${componentName}Frontend;
-  `;
+`;
 
   try {
     if (!fs.existsSync(REPO_DIR)) {
       console.log('Cloning GymSync-Frontend repo...');
-      await git.clone(`https://${TOKEN}@github.com/${USER_NAME}/GymSync-Frontend.git`, REPO_DIR);
+      await git.clone(REPO_URL, REPO_DIR);
     } else {
       console.log('Pulling latest changes...');
       await git.cwd(REPO_DIR).pull();
@@ -38,7 +38,7 @@ export default ${componentName}Frontend;
     await git.cwd(REPO_DIR)
       .addConfig('user.name', USER_NAME)
       .addConfig('user.email', `${USER_NAME}@autoflow.com`)
-      .add('./*')
+      .add('.')
       .commit(`Add ${fileName} via Autoflow`)
       .push('origin', 'main');
 
